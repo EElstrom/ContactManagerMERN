54efd0bbb0727310b5ba89f697d6640d6e04a615
devon-gadarowski <devon-gadarowski@knights.ucf.edu>
2020-01-26 11:10:27 -0500

    On branch development Your branch is up to date with 'origin/development'.
    Changes to be committed:
    	modified:   server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const path = require("path");
 const fetch = require("node-fetch");
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://devong:Shuckle@contactmanager-c6p1t.mongodb.net/test';
+const client = new MongoClient(url, {useUnifiedTopology: true});
+client.connect();
+
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "frontend", "build")));
@@ -17,23 +22,111 @@ app.use((req, res, next) =>
 });
 
 // API Endpoint for registering a new user account with the database
-	// Input : {"username":STRING, "password":STRING, "firstName":STRING, "lastname":STRING, "emailAddress":STRING}
+	// Input :    {"username":STRING, "password":STRING, "firstname":STRING, "lastname":STRING, "email":STRING}
 	// Response : {"result:STRING, "error":STRING}
 app.post('/api/register', async(req, res, next) =>
 {
+	function usernameIsValid(username)
+	{
+		return username != null && username.length > 4;
+	}
 
+	// Password validity will be checked before making the API request
+	function passwordIsValid(password)
+	{
+		return true;//password != null && password.length > 4;;
+	}
+
+	function emailIsValid(email)
+	{
+		return true;
+	}
+
+	try
+	{
+		const {username, password, firstname, lastname, email} = req.body;
+
+		// Check validity
+		if (!usernameIsValid(username))
+		{
+			var ret = {result:'failure', error:'invalid username'};
+			res.status(200).json(ret);
+			return;
+		}
+		if (!passwordIsValid(password))
+		{
+			var ret = {result:'failure', error:'invalid password'};
+			res.status(200).json(ret);
+			return;
+		}
+		if (!emailIsValid(email))
+		{
+			var ret = {result:'failure', error:'invalid email'};
+			res.status(200).json(ret);
+			return;
+		}
+
+
+		const db = client.db('contact_manager');
+
+		// Make sure username is not taken
+		const results = await db.collection('Users').find({username:username}).toArray();
+		if (results.length > 0)
+		{
+			var ret = {result:'failure', error:'username taken'};
+			res.status(200).json(ret);
+		}
+		else
+		{
+			// Register the new user
+			await db.collection('Users').insertOne({username:username, password:password, firstname:firstname, lastname:lastname, email:email});
+
+			var ret = {result:'success', error:''};
+			res.status(200).json(ret);
+		}
+	}
+	catch (e)
+	{
+		console.error(e);
+		var ret = {result:'failure', error:e.message};
+		res.status(200).json(ret);
+	}
 });
 
 // API Endpoint for logging in as an existing user
-	// Input : {"username":STRING, "password":STRING}
-	// Response : {"result:STRING, "error":STRING}
+	// Input :    {"username":STRING, "password":STRING}
+	// Response : {"result:STRING, "error":STRING, "firstname":STRING, "lastname":STRING}
 app.post('/api/login', async(req, res, next) =>
 {
+	try
+	{
+		const {username, password} = req.body;
 
+		const db = client.db('contact_manager');
+
+		// Attempt to login by searching the database for the correct username/password combo
+		const results = await db.collection('Users').find({username:username, password:password}).toArray();
+		if (results.length > 0)
+		{
+			var ret = {result:'success', error:'', firstname:results[0].firstname, lastname:results[0].lastname};
+			res.status(200).json(ret);
+		}
+		else
+		{
+			var ret = {result:'failure', error:'wrong login'};
+			res.status(200).json(ret);
+		}
+	}
+	catch (e)
+	{
+		console.error(e);
+		var ret = {result:'failure', error:e.message};
+		res.status(200).json(ret);
+	}	
 });
 
 // API Endpoint for communicating with cleverbot
-	// Input : {"message":STRING}
+	// Input :    {"message":STRING}
 	// Response : {"reply":STRING, "error":STRING}
 app.post('/api/clever', async(req, res, next) =>
 {
