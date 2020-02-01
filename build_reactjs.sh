bcef890f8897060904d2b8d2c4c8be56e4660f2c
Devon Gadarowski <devon.gadarowski@knights.ucf.edu>
2020-02-01 17:40:29 -0500

    On branch development Your branch is ahead of 'origin/development' by 1 commit.    (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   build_reactjs.sh
    	modified:   package.json

--- build_reactjs.sh
+++ build_reactjs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# update node packages
+(cd /var/www/ContactManagerApp/; npm install) > /dev/null 2> /dev/null
+(cd /var/www/ContactManagerApp/frontend/; npm install) > /dev/null 2> /dev/null
+
+# build react
 (cd /var/www/ContactManagerApp/frontend/; npm run build) > /dev/null 2> /dev/null
