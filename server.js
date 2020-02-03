e51c3cff2e87da83c9e558543bbf859bd6f4581b
Devon Gadarowski <devon.gadarowski@knights.ucf.edu>
2020-02-02 19:31:57 -0500

    On branch development Your branch is up to date with 'origin/development'.
    Changes to be committed:
    	modified:   server.js

--- server.js
+++ server.js
@@ -22,14 +22,6 @@ mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) =>
-{
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-	next();
-});
-
 // Frontend Routes
 app.use(express.static('./frontend/build'));
 app.get('*', function(req, res)
