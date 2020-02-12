19a8dc83d61ea5a08a5b6d3c06d6afc02a98e1cf
brooke <brookeshayb@gmail.com>
2020-02-12 13:55:09 -0500

    Centered homepage elements

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -67,7 +67,7 @@ class HomePage extends React.Component
 	{
 		return (
 			<div style={{display: 'block', position: 'fixed', width: '100vw', height: '100vh', overflow: 'auto'}}>
-				<div style={{display: 'flex', justifyContent: 'center', alignItem: 'center', position: 'fixed', width: '100vw'}}>
+				<div style={{display: 'flex', justifyContent: 'flex-start', alignItem: 'center', position: 'absolute', width: '100vw', left: '7%'}}>
 					{/*TODO: Add logo here?*/}
 					<input className='search-box' type='text' id='login' placeholder='search' onChange={(query) => this.searchContacts(query.target.value)}/>
 					<div className='rButtons'>
